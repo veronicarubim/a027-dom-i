5d1dde9c9ef4aa2de9ed4dc8a577213c872d21a6
veronicarubim <veronica.rubim97@gmail.com>
2022-10-03 16:19:30 -0300

    Exercicios aula27

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const frutas = ["laranja", "limão", "uva"];
+const frutas = ["laranja", "limão", "uva", "banana"];
+
+    let fruta1 = document.getElementById("fruta-1")
+    fruta1.innerHTML = frutas[0]
+
+    let fruta2 = document.getElementById("fruta-2")
+    fruta2.innerHTML = frutas[1]
+
+    let fruta3 = document.getElementById("fruta-3")
+    fruta3.innerHTML = frutas[2]
+
+    let fruta4 = document.getElementById("fruta-4")
+    fruta4.innerHTML = frutas[3]
+
+
+    function addFruta () {
+    const frutaDoInput = document.getElementById("fruta");
+    const lista = document.getElementById("lista-de-frutas")
+    lista.innerHTML += `<li>${frutaDoInput.value}</li>`
+    frutaDoInput.value = ""
+   }
+   
